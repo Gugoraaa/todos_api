5d8d248e4dba0de73d56463afe44721ad621537b
Gugoraaa <gus.gonzalez.ramos@gmail.com>
2025-03-31 21:33:15 -0700

    hotfix rollback

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Usa la URL de la base de datos en desarrollo
-const sql = neon(process.env.DATABASE_URL_UNPOOLED!);
+const sql = neon(process.env.DATABASE_URL!);
 
 export default sql;
